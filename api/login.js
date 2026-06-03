2d3b195f37cd3472627b46d6715ba70f058e9a7a
devnzzz <lowberthon@gmail.com>
2026-06-03 23:10:58 +0700

    Update and rename login.js.txt to login.js

--- api/login.js
+++ api/login.js
@@ -12,7 +12,7 @@ async function getDb() {
   return cachedClient.db(DB_NAME);
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
